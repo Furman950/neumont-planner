bbed10c2163b7f11609fe2cfb7bf7b9d099bc914
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-05-17 17:51:57 -0600

    Updated api to use https

--- mongo-rest-api/models/CalendarEvent.js
+++ mongo-rest-api/models/CalendarEvent.js
@@ -1,5 +1,9 @@
 module.exports = class CalendarEvent{
     constructor(title){
-        this.title = title
+        this.Name = name,
+        this.Description = description,
+        this.Id = Id,
+        this.Start = start,
+        this.End = end
     }
 }
