63a885163207bca66502262073e7079f95509b41
gt23669 <33846502+gt23669@users.noreply.github.com>
2019-05-24 16:51:25 -0600

    Modified week_view
    refactored code to properly display a new week when sliding left or right

--- lib/views/week_view.dart
+++ lib/views/week_view.dart
@@ -8,32 +8,46 @@ import 'package:neumont_planner/views/abstract_view.dart';
 import '../main.dart';
 
 class WeekView extends AbstractView {
-
   final DateTime date;
 
-  WeekView(List<Assignment> assignments, List<Course> courses, List<Event> events, changeView,this.date) : super(assignments, courses, events, changeView);
+  WeekView(List<Assignment> assignments, List<Course> courses,
+      List<Event> events, changeView, this.date)
+      : super(assignments, courses, events, changeView);
 
   @override
   Widget build(BuildContext context) {
     DateTime currentDate;
     List<String> dates = [];
+
     if (date == null) {
-      print("null");
       currentDate = DateTime.now();
     } else {
-      print("notNull");
       currentDate = date;
     }
-    DateTime _viewWeek =
+
+    DateTime _firstOfWeek =
         currentDate.subtract(new Duration(days: currentDate.weekday));
 
-    for (var i = 6, j = 0; j < 7; i++, j++) {
-      // print("${describeEnum(_WeekDay.values[i])} ${_thisWeek.month}/${(_thisWeek.day+j)}");
+    print(_firstOfWeek);
+
+    DateTime fromFirstOfWeek = _firstOfWeek;
+
+    // for (var i = 6, j = 0; j < 7; i++, j++) {
+    //   // print("${describeEnum(_WeekDay.values[i])} ${_thisWeek.month}/${(_thisWeek.day+j)}");
+    //   dates.add(
+    //       "${describeEnum(_WeekDay.values[i])}, ${_firstOfWeek.month}/${(_firstOfWeek.day + j)}");
+    //   if (i == 6) {
+    //     i = -1;
+    //   }
+    // }
+
+    for (var i = 0; i < 7; i++) {
+      DateTime temp = fromFirstOfWeek.add(new Duration(days: i));
+      // print(fromFirstOfWeek.add(new Duration(days: i)));
+      // dates.add(
+      //     "${describeEnum(_WeekDay.values[fromFirstOfWeek.weekday - 1])}, ${fromFirstOfWeek.month}/${fromFirstOfWeek.day}");
       dates.add(
-          "${describeEnum(_WeekDay.values[i])}, ${_viewWeek.month}/${(_viewWeek.day + j)}");
-      if (i == 6) {
-        i = -1;
-      }
+          "${describeEnum(_WeekDay.values[temp.weekday - 1])}, ${temp.month}/${temp.day}");
     }
 
     List<Container> cardList = buildCards(dates, assignments, events);
@@ -41,50 +55,6 @@ class WeekView extends AbstractView {
     double start = 0;
     double update = 0;
 
-    //   return Expanded(
-    //       child: ListView(
-    //           shrinkWrap: true,
-    //           children: dates
-    //               .map((dateString) => GestureDetector(
-    //                   onTap: () {
-    //                     changeView(View.DAY, currentDate);
-    //                   },
-    //                   onPanStart: (DragStartDetails details) {
-    //                     start = details.globalPosition.dx;
-    //                   },
-    //                   onPanUpdate: (DragUpdateDetails details) {
-    //                     update = details.globalPosition.dx - start;
-    //                   },
-    //                   onPanEnd: (DragEndDetails details) {
-    //                     if (update - start > 0) {
-    //                       //previoius
-    //                       print(currentDate.toString());
-    //                       DateTime toPass =
-    //                           currentDate.subtract(new Duration(days: 7));
-    //                       print(toPass.toString());
-    //                       changeView(View.WEEK, toPass);
-    //                     } else {
-    //                       //next
-    //                       DateTime toPass =
-    //                           currentDate.add(new Duration(days: 7));
-    //                       changeView(View.WEEK, toPass);
-    //                     }
-    //                   },
-    //                   child: Container(
-    //                       height: 100,
-    //                       child: Card(
-    //                         child: Column(
-    //                           children: assignments.length < 4
-    //                               ? <Widget>[
-    //                                   WeekDateHelper(assignments, dateString)
-    //                                 ]
-    //                               : <Widget>[
-    //                                   Text(
-    //                                       "$dateString\n${assignments.length.toString()} assignments")
-    //                                 ],
-    //                         ),
-    //                       ))))
-    //               .toList()));
     return Expanded(
       child: new GestureDetector(
         child: ListView(
