035f9a616bff30ce0d07a030c7daf0a47016abc2
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-05-30 15:45:01 -0600

    added more service stuff

--- lib/service/abstractServices/raspberry_pi_service.dart
+++ lib/service/abstractServices/raspberry_pi_service.dart
@@ -1,8 +1,9 @@
 import 'package:neumont_planner/models/objects/custom_event.dart';
 
 abstract class RaspberryPiService {
-  void createEvent(CustomEvent event, String userId);
-  void updateEvent(CustomEvent event, String userId);
-  void deleteEvent(String eventId, String userId);
-  Future<List<CustomEvent>> getAllEventsForUser(String userId);
+  void createEvent(CustomEvent event, String accessToken);
+  void updateEvent(CustomEvent event, String accessToken);
+  void deleteEvent(String eventId, String accessToken);
+  Future<CustomEvent> getEventById(String eventId, String accessToken);
+  Future<List<CustomEvent>> getAllEventsForUser(String accessToken);
 }
--- lib/service/raspberry_pi_api.dart
+++ lib/service/raspberry_pi_api.dart
@@ -1,5 +1,6 @@
 import 'dart:_http';
 import 'dart:async';
+import 'dart:io';
 
 import 'package:neumont_planner/models/objects/custom_event.dart';
 
@@ -10,31 +11,43 @@ class RaspberryPiAPI implements RaspberryPiService {
   String _baseEndPoint = "https://ferminsandoval.com:5585/api/events/";
 
   @override
-  void createEvent(CustomEvent event, String userId) {
-    
+  void createEvent(CustomEvent event, String accessToken) async {
+    await http.post(_baseEndPoint + "new",
+    body: event.toJson(),
+    headers: {HttpHeaders.authorizationHeader: "Bearer " + accessToken});
   }
 
   @override
-  void deleteEvent(String eventId, String userId) {
-    // TODO: implement deleteEvent
+  void deleteEvent(String eventId, String accessToken) async {
+    await http.delete(_baseEndPoint + "delete/" + eventId,
+        headers: {HttpHeaders.authorizationHeader: "Bearer " + accessToken});
   }
 
   @override
-  Future<List<CustomEvent>> getAllEventsForUser(String userId) async {
-    final response = await http.get(
-      _baseEndPoint + "all",
-      headers: { HttpHeaders.authorizationHeader: "Bearer " + userId }
-    );
+  Future<List<CustomEvent>> getAllEventsForUser(String accessToken) async {
+    final response = await http.get(_baseEndPoint + "all",
+        headers: {HttpHeaders.authorizationHeader: "Bearer " + accessToken});
 
     var completer = new Completer<List<CustomEvent>>();
     completer.complete(eventsFromJson(response.body));
-    
+
     return completer.future;
   }
 
   @override
-  void updateEvent(CustomEvent event, String userId) {
-    // TODO: implement updateEvent
+  void updateEvent(CustomEvent event, String accessToken) async {
+    await http.patch(_baseEndPoint + "edit/" + event.id,
+        headers: {HttpHeaders.authorizationHeader: "Bearer " + accessToken});
+  }
+
+  @override
+  Future<CustomEvent> getEventById(String eventId, String accessToken) async {
+    final response = await http.get(_baseEndPoint + eventId,
+        headers: {HttpHeaders.authorizationHeader: "Bearer " + accessToken});
+
+    var completer = new Completer<CustomEvent>();
+    completer.complete(eventsFromJson(response.body)[0]);
+
+    return completer.future;
   }
 }
-
